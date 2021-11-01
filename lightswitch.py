7a30220f36a0c306e3839859361562e6b75dc09d
= <lukieluuk2@gmail.com>
2021-11-01 09:43:16 +0100

    lightswitch python

---
+++ lightswitch.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+window = tk.Tk()
+
+color = False
+def switchColor():
+    global color
+    color = not color
+    if color:
+        button.configure(bg="yellow")
+    elif not color:
+        button.configure(bg="black")
+
+button = tk.Button(text='...', bg="white", fg="black", command = switchColor)
+button.pack(pady = 20, padx = 20)
+
+# schijf hier tussen je code
+
+
+
+# schijf hier tussen je code
+
+window.mainloop()
